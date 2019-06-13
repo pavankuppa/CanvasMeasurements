80d361cb9448e62b9eb5c2e23c2c5fd51da3c40a
Pavan Kuppa <pavan.kuppa@hotmail.com>
2019-06-13 17:39:57 +0530

    Setup Package

--- src/Measurement.ts
+++ src/Measurement.ts
@@ -1,13 +1,22 @@
 class Measurement {
-    canvas: HTMLCanvasElement;
-    context: any;
+    public canvas: HTMLCanvasElement;
+    public context: any;
 
-    constructor(canvas: HTMLCanvasElement){
-        this.canvas = canvas
-        this.context = canvas.getContext("2d")
+    constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas;
+        this.context = canvas.getContext('2d');
     }
 
-    ellipse({ x, y, radiusX, radiusY, rotation, startAngle, endAngle, anticlockwise = false }: {
+    public ellipse({
+        x,
+        y,
+        radiusX,
+        radiusY,
+        rotation,
+        startAngle,
+        endAngle,
+        anticlockwise = false,
+    }: {
         x: number;
         y: number;
         radiusX: number;
@@ -16,50 +25,57 @@ class Measurement {
         startAngle: number;
         endAngle: number;
         anticlockwise?: boolean;
-    }){
+    }) {
         this.context.beginPath();
         this.context.ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle, anticlockwise);
         this.context.stroke();
     }
 
-    circle({x, y, radius, startAngle, endAngle, anticlockwise = false}: {
+    public circle({
+        x,
+        y,
+        radius,
+        startAngle,
+        endAngle,
+        anticlockwise = false,
+    }: {
         x: number;
         y: number;
         radius: number;
         startAngle: number;
         endAngle: number;
         anticlockwise?: boolean;
-    }){
+    }) {
         this.context.beginPath();
         this.context.arc(x, y, radius, startAngle, endAngle, anticlockwise);
         this.context.stroke();
     }
 
-    rectangle({x, y, width, height}: {
-        x: number, y: number, width: number, height: number
-    }){
-        this.context.beginPath()
+    public rectangle({ x, y, width, height }: { x: number; y: number; width: number; height: number }) {
+        this.context.beginPath();
         this.context.rect(x, y, width, height);
         this.context.stroke();
     }
 
-    square({x, y, side}: {
-        x: number, y: number, side: number
-    }){
-        this.context.beginPath()
+    public square({ x, y, side }: { x: number; y: number; side: number }) {
+        this.context.beginPath();
         this.context.rect(x, y, side, side);
         this.context.stroke();
     }
 
-    line({ from, to }: {
+    public line({
+        from,
+        to,
+    }: {
         from: {
             x: number;
             y: number;
-        }; to: {
+        };
+        to: {
             x: number;
             y: number;
         };
-    }){
+    }) {
         this.context.beginPath();
         this.context.moveTo(from.x, from.y);
         this.context.lineTo(to.x, to.y);
